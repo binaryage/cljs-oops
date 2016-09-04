687a00758b237110d3b95bfae381ec76a9473c76
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-04 15:35:58 +0200

    reimplement debug log on top of clojure.pprint

--- src/lib/oops/debug.clj
+++ src/lib/oops/debug.clj
@@ -1,5 +1,10 @@
-(ns oops.debug)
+(ns oops.debug
+  (:require [clojure.pprint]))
+
+(defn pprint [v]
+  (with-out-str
+    (clojure.pprint/pprint v)))
 
 (defn log [& args]
-  (let [msg (apply str (interpose " " args))]
+  (let [msg (apply str (interpose " " (map pprint args)))]
     (.print System/out (str msg "\n"))))
