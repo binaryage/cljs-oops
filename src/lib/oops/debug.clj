e1921433a3c4770d58c56a7b8054cbd4801996fe
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-04 16:54:21 +0200

    implement better new-line handling in debug log

--- src/lib/oops/debug.clj
+++ src/lib/oops/debug.clj
@@ -1,9 +1,16 @@
 (ns oops.debug
-  (:require [clojure.pprint]))
+  (:require [clojure.pprint]
+            [clojure.string :as string]))
+
+(defn trimr-nl [s]
+  (string/replace s #"\n$" ""))
 
 (defn pprint [v]
-  (with-out-str
-    (clojure.pprint/pprint v)))
+  (if (= v "\n")
+    v
+    (trimr-nl (with-out-str
+                (binding [clojure.pprint/*print-right-margin* 200]
+                  (clojure.pprint/pprint v))))))
 
 (defn log [& args]
   (let [msg (apply str (interpose " " (map pprint args)))]
