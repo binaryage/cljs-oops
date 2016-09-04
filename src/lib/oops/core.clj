301995451c3a2fbf2ef59f9aa28eb3988d276adc
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-04 12:48:01 +0200

    simplify dynamic macros because we know we are getting symbols as inputs

--- src/lib/oops/core.clj
+++ src/lib/oops/core.clj
@@ -15,15 +15,13 @@
   `(dynamic-selector-fetch ~o ~@selector))
 
 (defmacro dynamic-key-fetch-impl [o key]
-  (let [o-sym (gensym)
-        key-sym (gensym)]
-    `(let [~o-sym ~o
-           ~key-sym ~key]
-       (if (sequential? ~key-sym)
-         (apply dynamic-selector-fetch ~o-sym ~key-sym)
-         (do
-           (assert (or (string? ~key-sym)) (keyword? ~key-sym))                                                               ; TODO: this should be validated by specs on cljs side
-           ~(gen-key-fetch o-sym `(name ~key-sym)))))))
+  {:pre [(symbol? o)
+         (symbol? key)]}
+  `(if (sequential? ~key)
+     (apply dynamic-selector-fetch ~o ~key)
+     (do
+       (assert (or (string? ~key)) (keyword? ~key))                                                                           ; TODO: this should be validated by specs on cljs side
+       ~(gen-key-fetch o `(name ~key)))))
 
 (defn gen-dynamic-selector-validation [selector]
   `(if-not (clojure.spec/valid? ::oops.sdefs/obj-selector ~selector)
@@ -31,17 +29,15 @@
                      {:explain (clojure.spec/explain-data ::oops.sdefs/obj-selector ~selector)}))))
 
 (defmacro dynamic-selector-fetch-impl [o selector]
-  {:pre [(symbol? selector)
-         (symbol? o)]}
+  {:pre [(symbol? o)
+         (symbol? selector)]}
   `(do
      ~(gen-dynamic-selector-validation selector)
-     (let [selector# ~selector
-           o# ~o]
-       (if (empty? selector#)
-         o#
-         (let [next-o# (dynamic-key-fetch o# (first selector#))
-               remaining-selector# (rest selector#)]
-           (recur next-o# remaining-selector#))))))
+     (if (empty? ~selector)
+       ~o
+       (let [next-o# (dynamic-key-fetch ~o (first ~selector))
+             remaining-selector# (rest ~selector)]
+         (recur next-o# remaining-selector#)))))
 
 (defmacro oget
   ([o & selector]
