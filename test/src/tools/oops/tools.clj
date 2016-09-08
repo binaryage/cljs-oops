bb44a288e461ed9c079e597cdb34a2ec68bca4e8
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-08 19:01:19 +0200

    add `runonce` helper macro to tools

--- test/src/tools/oops/tools.clj
+++ test/src/tools/oops/tools.clj
@@ -53,3 +53,10 @@
      ~(gen-devtools-if-needed)
      (emit-arena-separator!)))
 
+(defmacro runonce [& body]
+  (let [code (cons 'do body)
+        code-string (pr-str code)
+        code-hash (hash code-string)
+        name (symbol (str "runonce_" code-hash))]
+    `(defonce ~name {:value ~code
+                     :code  ~code-string})))
