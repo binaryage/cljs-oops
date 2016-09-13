5382fbac81eedbf1cf4e586a88b87b6d6cd87e69
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-13 21:49:49 +0200

    make sdefs more structured

--- src/lib/oops/sdefs.clj
+++ src/lib/oops/sdefs.clj
@@ -4,8 +4,12 @@
 
 ; --- specs -----------------------------------------------------------------------------------------------------------------
 
-(s/def ::obj-key (s/or :string string? :keyword keyword?))
-(s/def ::obj-selector (s/or :key ::obj-key :selector (s/* ::obj-selector)))
+(s/def ::obj-key (s/or :string string?
+                       :keyword keyword?))
+(s/def ::obj-selector (s/or :key ::obj-key
+                            :selector (s/* ::obj-selector)))
 
-(s/def ::obj-path-item (s/tuple #{dot-access soft-access punch-access} string?))
+(s/def ::obj-path-key string?)
+(s/def ::obj-path-mode #{dot-access soft-access punch-access})
+(s/def ::obj-path-item (s/tuple ::obj-path-mode ::obj-path-key))
 (s/def ::obj-path (s/* ::obj-path-item))
--- src/lib/oops/sdefs.cljs
+++ src/lib/oops/sdefs.cljs
@@ -5,15 +5,18 @@
 
 ; --- specs -----------------------------------------------------------------------------------------------------------------
 
-(s/def ::obj-key (s/or :string string? :keyword keyword?))
-(s/def ::obj-selector (s/or :key ::obj-key :selector (native-array-aware-* ::obj-selector)))
-
-(s/def ::obj-path-item (s/tuple #{(get-dot-access) (get-soft-access) (get-punch-access)} string?))
+(s/def ::obj-key (s/or :string string?
+                       :keyword keyword?))
+(s/def ::obj-selector (s/or :key ::obj-key
+                            :selector (native-array-aware-* ::obj-selector)))
 
 ; note: ::obj-path is a native array for perfomance reasons
 ;       it is not a sequence of tuples, but it is flat sequence of pairs mode-key
 ;       for an example clj path [[0 "key1"] [0 "key2"] ...]
 ;       the equivalent cljs path is [0 "key1" 0 "key2"]
+(s/def ::obj-path-mode #{(get-dot-access) (get-soft-access) (get-punch-access)})
+(s/def ::obj-path-key string?)
+(s/def ::obj-path-item (s/tuple ::obj-path-mode ::obj-path-key))
 (s/def ::obj-path (s/and array? (fn [arr]
                                   (let [pairs (map vec (partition-all 2 arr))]
                                     (s/valid? (s/* ::obj-path-item) pairs)))))
