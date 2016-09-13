001f53b63b62e78eef64920b7a3c93e16cdc28a4
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-13 21:43:08 +0200

    introduce oops.spec-helpers and hide some cljs spec code there

--- src/lib/oops/sdefs.cljs
+++ src/lib/oops/sdefs.cljs
@@ -1,15 +1,19 @@
 (ns oops.sdefs
-  (:require-macros [oops.constants :refer [get-dot-access get-soft-access get-punch-access]])
+  (:require-macros [oops.constants :refer [get-dot-access get-soft-access get-punch-access]]
+                   [oops.spec-helpers :refer [native-array-aware-*]])
   (:require [clojure.spec :as s]))
 
 ; --- specs -----------------------------------------------------------------------------------------------------------------
 
 (s/def ::obj-key (s/or :string string? :keyword keyword?))
-(s/def ::obj-array-selector (s/and array? (fn [arr]
-                                            (every? #(s/valid? ::obj-selector %) arr))))
-(s/def ::obj-selector (s/or :key ::obj-key :selector (s/* ::obj-selector) :array ::obj-array-selector))
+(s/def ::obj-selector (s/or :key ::obj-key :selector (native-array-aware-* ::obj-selector)))
 
 (s/def ::obj-path-item (s/tuple #{(get-dot-access) (get-soft-access) (get-punch-access)} string?))
+
+; note: ::obj-path is a native array for perfomance reasons
+;       it is not a sequence of tuples, but it is flat sequence of pairs mode-key
+;       for an example clj path [[0 "key1"] [0 "key2"] ...]
+;       the equivalent cljs path is [0 "key1" 0 "key2"]
 (s/def ::obj-path (s/and array? (fn [arr]
                                   (let [pairs (map vec (partition-all 2 arr))]
                                     (s/valid? (s/* ::obj-path-item) pairs)))))
