a5e6a3331ef9f1a2126b90317df2bdae130cdcd6
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-15 20:08:48 +0200

    implement `with-compiler-config` for ad-hoc compiler config tweaks

--- src/lib/oops/config.clj
+++ src/lib/oops/config.clj
@@ -36,7 +36,13 @@
 (def advanced-mode-compiler-config-overrides
   {:diagnostics false})
 
+; this is for testing, see with-compiler-config macro below
+(def adhoc-config-overrides (volatile! {}))
+
 ; -- helpers ----------------------------------------------------------------------------------------------------------------
+
+(defn get-adhoc-config-overrides []
+  @adhoc-config-overrides)
 
 (defn advanced-mode? []
   (if cljs.env/*compiler*
@@ -54,7 +60,7 @@
 
 (defn ^:dynamic get-compiler-config []
   {:post [(map? %)]}                                                                                                          ; TODO: validate config using spec or bhauman's tooling
-  (merge (prepare-default-config) (read-project-config) (read-env-config)))
+  (merge (prepare-default-config) (read-project-config) (read-env-config) (get-adhoc-config-overrides)))
 
 ; -- public api--------------------------------------------------------------------------------------------------------------
 
@@ -67,6 +73,30 @@
               [(keyword (second m)) val]))]
     ; select all :runtime-something keys and drop :runtime- prefix
     (into {} (keep * (or config (get-current-compiler-config))))))
+
+; -- updates to compiler config during compilation --------------------------------------------------------------------------
+
+(def adhoc-config-overrides-stack (volatile! []))
+
+(defmacro push-config-overrides! [config]
+  (let [current-adhoc-config-overrides @adhoc-config-overrides]
+    (vreset! adhoc-config-overrides-stack (conj @adhoc-config-overrides-stack current-adhoc-config-overrides))
+    (vreset! adhoc-config-overrides (merge current-adhoc-config-overrides config))))
+
+(defmacro pop-config-overrides! []
+  (let [stack @adhoc-config-overrides-stack]
+    (assert (pos? (count stack)))
+    (vreset! adhoc-config-overrides (last stack))
+    (vreset! adhoc-config-overrides-stack (butlast stack))))
+
+; note: we rely on macroexpand behaviour here, it will expand
+; push-compiler-config first, then macros in body and then pop-compiler-config
+; this way we can tweak compiler config for an isolated code piece, which is handy for testing
+(defmacro with-compiler-config [config & body]
+  `(do
+     (oops.config/push-config-overrides! ~config)
+     ~@body
+     (oops.config/pop-config-overrides!)))
 
 ; -- runtime macros ---------------------------------------------------------------------------------------------------------
 
