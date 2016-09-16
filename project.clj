6c5a3a5451115e86b4ef42f6b79148a41aaf39f8
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-16 17:11:40 +0200

    project: make sure we declare proper dependencies

--- project.clj
+++ project.clj
@@ -10,15 +10,15 @@
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha12" :scope "provided"]
                  [org.clojure/clojurescript "1.9.229" :scope "provided"]
+                 [funcool/cuerdas "1.0.1"]
+                 [environ "1.1.0"]
 
                  [binaryage/devtools "0.8.1" :scope "test"]
                  [binaryage/dirac "0.6.6" :scope "test"]
                  [figwheel "0.5.7" :scope "test"]
                  [org.clojure/tools.logging "0.3.1" :scope "test"]
                  [clj-logging-config "1.9.12" :scope "test"]
-                 [environ "1.1.0" :scope "test"]
-                 [clansi "1.0.0" :scope "test"]
-                 [funcool/cuerdas "1.0.1" :scope "test"]]
+                 [clansi "1.0.0" :scope "test"]]
 
   :clean-targets ^{:protect false} ["target"
                                     "test/resources/_compiled"]
