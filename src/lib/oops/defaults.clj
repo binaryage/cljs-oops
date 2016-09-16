e8af60ddb3143333d2f6504077f2805561e25ccb
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-16 15:35:01 +0200

    write specs for validating config

--- src/lib/oops/defaults.clj
+++ src/lib/oops/defaults.clj
@@ -1,6 +1,7 @@
 (ns oops.defaults
   (:require [cljs.env]
-            [oops.state]))
+            [oops.state]
+            [clojure.spec :as s]))
 
 (def config                                                                                                                   ; falsy below means 'nil' or 'false'
   {; -- compiler config -----------------------------------------------------------------------------------------------------
@@ -8,6 +9,7 @@
    :key-get                         :goog                                                                                     ; #{:core :goog}
    :key-set                         :goog                                                                                     ; #{:core :goog}
    :strict-punching                 true                                                                                      ; #{true falsy}
+   :skip-config-validation          false                                                                                     ; #{true falsy}
 
    ; compile-time warnings/errors
    :dynamic-selector-usage          :warn                                                                                     ; #{:error :warn falsy}
@@ -35,3 +37,60 @@
 
 (def advanced-mode-config-overrides
   {:diagnostics false})
+
+; -- config validation specs ------------------------------------------------------------------------------------------------
+
+; please note that we want this code to be co-located with default config for easier maintenance
+; but formally we want config spec to reside in oops.config namespace
+(alias 'config 'oops.config)
+
+; -- config helpers ---------------------------------------------------------------------------------------------------------
+
+(s/def ::config/boolish #(contains? #{true false nil} %))
+(s/def ::config/key-impl #(contains? #{:goog :core} %))
+(s/def ::config/message #(contains? #{:error :warn false nil} %))
+(s/def ::config/reporting #(contains? #{:throw :console false nil} %))
+(s/def ::config/child-factory #(contains? #{:js-obj :js-array} %))
+
+; -- config keys ------------------------------------------------------------------------------------------------------------
+
+(s/def ::config/diagnostics ::config/boolish)
+(s/def ::config/key-get ::config/key-impl)
+(s/def ::config/key-set ::config/key-impl)
+(s/def ::config/strict-punching ::config/boolish)
+(s/def ::config/skip-config-validation ::config/boolish)
+
+(s/def ::config/dynamic-selector-usage ::config/message)
+(s/def ::config/static-nil-target-object ::config/message)
+(s/def ::config/static-empty-selector-access ::config/message)
+
+(s/def ::config/runtime-unexpected-object-value ::config/message)
+(s/def ::config/runtime-invalid-selector ::config/message)
+(s/def ::config/runtime-missing-object-key ::config/message)
+(s/def ::config/runtime-empty-selector-access ::config/message)
+
+(s/def ::config/runtime-error-reporting ::config/reporting)
+(s/def ::config/runtime-warning-reporting ::config/reporting)
+
+(s/def ::config/runtime-child-factory ::config/child-factory)
+
+(s/def ::config/debug ::config/boolish)
+
+; -- config map -------------------------------------------------------------------------------------------------------------
+
+(s/def ::config/config
+  (s/keys :req-un [::config/diagnostics
+                   ::config/key-get
+                   ::config/key-set
+                   ::config/strict-punching
+                   ::config/dynamic-selector-usage
+                   ::config/static-nil-target-object
+                   ::config/static-empty-selector-access
+                   ::config/runtime-unexpected-object-value
+                   ::config/runtime-invalid-selector
+                   ::config/runtime-missing-object-key
+                   ::config/runtime-empty-selector-access
+                   ::config/runtime-error-reporting
+                   ::config/runtime-warning-reporting
+                   ::config/runtime-child-factory
+                   ::config/debug]))
