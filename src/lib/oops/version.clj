1559322ec8b171ba4fcf0f5fa1af74605881c799
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-20 23:28:01 +0200

    project: move to next snapshot version

--- src/lib/oops/version.clj
+++ src/lib/oops/version.clj
@@ -1,6 +1,6 @@
 (ns oops.version)
 
-(def current-version "0.2.0")                                                                                        ; this should match our project.clj
+(def current-version "0.2.1-SNAPSHOT")                                                                                        ; this should match our project.clj
 
 (defmacro get-current-version []
   current-version)
