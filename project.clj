a45757e98194b1d95bbf85c53ab24ca5ff05f0a5
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-21 16:08:35 +0200

    project: move to next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/oops "0.3.0"
+(defproject binaryage/oops "0.3.1-SNAPSHOT"
   :description "ClojureScript macros for convenient Javascript object access."
   :url "https://github.com/binaryage/cljs-oops"
   :license {:name         "MIT License"
