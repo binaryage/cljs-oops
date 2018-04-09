ea43b80a27a27a2b84434168f20b7d9d411632ee
Antonin Hildebrand <antonin@hildebrand.cz>
2018-04-09 14:16:40 +0200

    version to 0.6.1

--- src/lib/oops/version.clj
+++ src/lib/oops/version.clj
@@ -1,6 +1,6 @@
 (ns oops.version)
 
-(def current-version "0.6.0")                                                                                                 ; this should match our project.clj
+(def current-version "0.6.1")                                                                                                 ; this should match our project.clj
 
 (defmacro get-current-version []
   current-version)
